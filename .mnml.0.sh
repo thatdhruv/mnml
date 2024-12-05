21739ae8407730fff1ef7b401fcc313bac2d1736
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2024-12-05 15:13:05 +0530

    Update .mnml.0.sh

--- .mnml.0.sh
+++ .mnml.0.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 
 source $MNMLDIR/.mnml.conf
+centered_message "MNML Phase 0 - Started\n"
+
 iso=$(curl -4 ifconfig.co/country-iso)
 timedatectl set-ntp true
 pacman -S --needed --noconfirm archlinux-keyring
 sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
 pacman -S --needed --noconfirm reflector rsync grub
 
-echo -ne "
-\033[0;31m[setting up $iso mirrors for optimal downloads]\033[0m
-"
+centered_message "[setting up $iso mirrors for optimal downloads]"
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
 reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 mkdir /mnt &>/dev/null
 
-echo -ne "
-\033[0;31m[formatting disks]\033[0m
-"
+centered_message "[formatting disks]"
 umount -A --recursive /mnt
 sgdisk -Z ${MNMLDISK}
 sgdisk -a 2048 -o ${MNMLDISK}
@@ -28,10 +26,7 @@ if [[ ! -d "/sys/firmware/efi" ]] ; then
 fi
 partprobe ${MNMLDISK}
 
-
-echo -ne "
-\033[0;31m[creating filesystems]\033[0m
-"
+centered_message "[creating filesystems]"
 if [[ "${MNMLDISK}" =~ "nvme" ]] ; then
 	partition2=${MNMLDISK}p2
 	partition3=${MNMLDISK}p3
@@ -46,35 +41,28 @@ mkdir -p /mnt/boot/efi
 mount -t vfat -L EFIBOOT /mnt/boot/
 if ! grep -qs '/mnt' /proc/mounts ; then
 	clear
-	echo -ne "\033[0;31m[disk is not mounted!]\033[0m"
-	echo
-	echo -ne "\033[0;31m[rebooting in 3s...]\033[0m" && sleep 1
-	echo -ne "\033[0;31m[rebooting in 2s...]\033[0m" && sleep 1
-	echo -ne "\033[0;31m[rebooting in 1s...]\033[0m" && sleep 1
+	centered_message "[disk is not mounted!]\n"
+	centered_message "[rebooting in 3s...]" && sleep 1
+	centered_message "[rebooting in 2s...]" && sleep 1
+	centered_message "[rebooting in 1s...]" && sleep 1
 	reboot now
 fi
 
-echo -ne "
-\033[0;31m[installing the base system]\033[0m
-"
+centered_message "[installing the base system]"
 pacstrap /mnt base base-devel linux linux-firmware archlinux-keyring sudo vim --needed --noconfirm
 echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
 cp -R ${MNMLDIR} /mnt/root/MNML
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 genfstab -L /mnt >> /mnt/etc/fstab
 
-echo -ne "
-\033[0;31m[installing bootloader]\033[0m
-"
+centered_message "[installing bootloader]"
 if [[ ! -d "/sys/firmware/efi" ]] ; then
 	grub-install --boot-directory=/mnt/boot ${MNMLDISK}
 else
 	pacstrap /mnt efibootmgr --needed --noconfirm
 fi
 
-echo -ne "
-\033[0;31m[checking if a swap file will be needed...]\033[0m
-"
+centered_message "[checking if a swap file will be needed...]"
 MNMLMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
 if [[ MNMLMEM -lt 8000000 ]] ; then
 	mkdir -p /mnt/opt/swap
@@ -86,6 +74,4 @@ if [[ MNMLMEM -lt 8000000 ]] ; then
 	echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab
 fi
 
-echo -ne "
-\033[0;31m[ready for phase 1]\033[0m
-"
+centered_message "MNML Phase 0 - Completed! Proceeding Towards Phase 1..."
