35e89455a69d59e20bd6958df1b79e72ee79c94f
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2024-12-06 00:07:07 +0530

    Update .mnml.2.sh

--- .mnml.2.sh
+++ .mnml.2.sh
@@ -4,19 +4,19 @@ source $HOME/mnml/.mnml.conf
 centered_message "MNML Phase 2 - Started\n"
 
 centered_message "[setting up graphical environment]"
-cd $MNMLDIR/dmenu
+cd $HOME/mnml/dmenu
 make
 sudo make install
-cd $MNMLDIR/dwm
+cd $HOME/mnml/dwm
 make
 sudo make install
-cd $MNMLDIR/slstatus
+cd $HOME/mnml/slstatus
 make
 sudo make install
-cd $MNMLDIR/st
+cd $HOME/mnml/st
 make
 sudo make install
-cp -r $MNMLDIR/dotfiles/. $HOME/.
+cp -r $HOME/mnml/dotfiles/. $HOME/.
 chmod +x $HOME/.scripts/*.sh
 
 centered_message "MNML Phase 2 - Completed! Proceeding Towards Phase 3..."
