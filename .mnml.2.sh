09dc19ae9e83afcd4e97d0826bbfbc7e325f4df3
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2025-03-17 00:16:45 -0500

    Update .mnml.2.sh

--- .mnml.2.sh
+++ .mnml.2.sh
@@ -20,4 +20,6 @@ sudo make install
 cp -r $HOME/mnml/dotfiles/. $HOME/.
 chmod +x $HOME/.scripts/*.sh
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 centered_message "MNML Phase 2 - Completed! Proceeding Towards Phase 3..."
