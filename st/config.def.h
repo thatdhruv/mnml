91bdbf8ab8e3789fc04989795915f929793c4fc9
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2025-03-17 20:15:43 -0500

    Update config.def.h

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font Mono:style=bold:pixelsize=24:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font Mono:style=regular:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
  
 /*
