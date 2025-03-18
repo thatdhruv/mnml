d6d7c9647b1419534078e348b11ca2107e8d5797
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2025-03-17 19:17:13 -0500

    Update config.def.h

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminess Nerd Font Mono:style=bold:pixelsize=24:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font Mono:style=bold:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 2;
  
 /*
