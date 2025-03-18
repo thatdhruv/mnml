d6a6a7e0b629b843c18c937b9325d1282dcf5392
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2025-03-17 19:16:58 -0500

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminess Nerd Font Mono:style=bold:size=16" };
-static const char dmenufont[]       = "Terminess Nerd Font Mono:style=bold:size=16";
+static const char *fonts[]          = { "JetBrains Mono Nerd Font Mono:style=bold:size=14" };
+static const char dmenufont[]       = "JetBrains Mono Nerd Font Mono:style=bold:size=14";
 
 static const char col_acc_dark[]    = "#cc6666";
 static const char col_bg_dark[]     = "#222222";
